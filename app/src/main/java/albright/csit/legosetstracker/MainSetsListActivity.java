9714b8c2c990e0500835433d4cf43ad7642b4538
King-Nivek <King-Nivek@users.noreply.github.com>
2015-11-08 13:02:23 -0500

    Added onCreateOptionsMenu function for MainSetsListActivity.java

--- app/src/main/java/albright/csit/legosetstracker/MainSetsListActivity.java
+++ app/src/main/java/albright/csit/legosetstracker/MainSetsListActivity.java
@@ -2,6 +2,8 @@ package albright.csit.legosetstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainSetsListActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainSetsListActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_sets_list);
   }
+
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
 }
