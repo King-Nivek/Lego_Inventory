f67aad4ffe71a596dcfc78817fbdee33f55566f9
King-Nivek <King-Nivek@users.noreply.github.com>
2015-11-11 19:34:29 -0500

    Updated MainSetsListActivity.java to extend from ActivityMenu.

--- app/src/main/java/albright/csit/legosetstracker/MainSetsListActivity.java
+++ app/src/main/java/albright/csit/legosetstracker/MainSetsListActivity.java
@@ -1,22 +1,14 @@
 package albright.csit.legosetstracker;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 
-public class MainSetsListActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class MainSetsListActivity extends ActivityMenu {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_sets_list);
-  }
-
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.main_menu, menu);
-    return true;
   }
 
 }
