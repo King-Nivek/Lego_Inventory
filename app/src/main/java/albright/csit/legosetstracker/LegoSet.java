aacc0d8f2d7501d8ec804ce7678ab6a4cb0c2c63
King-Nivek <King-Nivek@users.noreply.github.com>
2015-11-11 20:05:47 -0500

    First iteration of code for LegoSet.java

--- app/src/main/java/albright/csit/legosetstracker/LegoSet.java
+++ app/src/main/java/albright/csit/legosetstracker/LegoSet.java
@@ -6,10 +6,112 @@ Creation Date:  11.11.2015
 Last Modified:  11.11.2015
 
    Assignment:  Lego Sets Tracker
-    File Name:  LegoSet
+    File Name:  LegoSet.java
       Purpose:  
 ______________________________________________________________________________*/
+
 package albright.csit.legosetstracker;
 
 public class LegoSet {
+  ////  Fields
+  /////////////////////////////////////
+  private int autoId;
+  private String legoSetId;
+  private String legoSetName;
+  private int legoThemeId;
+  private String legoThemeName;
+  private int legoSetPieces;
+  private String legoSetAcquiredDate;
+  private int legoSetQuantity;
+
+  ////  Setters
+  /////////////////////////////////////
+  public void setAutoId(int autoId) {
+    this.autoId = autoId;
+  }
+
+  public void setLegoSetId(String legoSetId) {
+    this.legoSetId = legoSetId;
+  }
+
+  public void setLegoSetName(String legoSetName) {
+    this.legoSetName = legoSetName;
+  }
+
+  public void setLegoThemeId(int legoThemeId) {
+    this.legoThemeId = legoThemeId;
+  }
+
+  public void setLegoThemeName(String legoThemeName) {
+    this.legoThemeName = legoThemeName;
+  }
+
+  public void setLegoSetPieces(int legoSetPieces) {
+    this.legoSetPieces = legoSetPieces;
+  }
+
+  public void setLegoSetAcquiredDate(String legoSetAcquiredDate) {
+    this.legoSetAcquiredDate = legoSetAcquiredDate;
+  }
+
+  public void setLegoSetQuantity(int legoSetQuantity) {
+    this.legoSetQuantity = legoSetQuantity;
+  }
+
+  ////  Getters
+  /////////////////////////////////////
+  public int getAutoId() {
+    return autoId;
+  }
+
+  public String getLegoSetId() {
+    return legoSetId;
+  }
+
+  public String getLegoSetName() {
+    return legoSetName;
+  }
+
+  public int getLegoThemeId() {
+    return legoThemeId;
+  }
+
+  public String getLegoThemeName() {
+    return legoThemeName;
+  }
+
+  public int getLegoSetPieces() {
+    return legoSetPieces;
+  }
+
+  public String getLegoSetAcquiredDate() {
+    return legoSetAcquiredDate;
+  }
+
+  public int getLegoSetQuantity() {
+    return legoSetQuantity;
+  }
+
+  ////  Constructors
+  /////////////////////////////////////
+  public LegoSet() {
+  }
+
+  public LegoSet(int autoId) {
+    this.autoId = autoId;
+  }
+
+  public LegoSet(int autoId, String legoSetId, String legoSetName,
+                 int legoThemeId, String legoThemeName, int legoSetPieces,
+                 String legoSetAcquiredDate, int legoSetQuantity) {
+
+    this.autoId = autoId;
+    this.legoSetId = legoSetId;
+    this.legoSetName = legoSetName;
+    this.legoThemeId = legoThemeId;
+    this.legoThemeName = legoThemeName;
+    this.legoSetPieces = legoSetPieces;
+    this.legoSetAcquiredDate = legoSetAcquiredDate;
+    this.legoSetQuantity = legoSetQuantity;
+  }
 }
