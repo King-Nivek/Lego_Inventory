8cf5cc5312d36720c9ee8fa24f4a54485cd206c7
King-Nivek <King-Nivek@users.noreply.github.com>
2015-11-11 19:58:55 -0500

    Initial Commit for LegoSet.java

---
+++ app/src/main/java/albright/csit/legosetstracker/LegoSet.java
@@ -0,0 +1,15 @@
+/*______________________________________________________________________________
+   Created By:  Kevin M. Albright
+Creation Date:  11.11.2015
+
+  Modified By:  Kevin M. Albright
+Last Modified:  11.11.2015
+
+   Assignment:  Lego Sets Tracker
+    File Name:  LegoSet
+      Purpose:  
+______________________________________________________________________________*/
+package albright.csit.legosetstracker;
+
+public class LegoSet {
+}
