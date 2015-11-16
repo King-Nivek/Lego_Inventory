ffa0bab90a5e0ef16e66be47f336f44ec13b1949
King-Nivek <King-Nivek@users.noreply.github.com>
2015-11-16 17:54:34 -0500

    Initial Commit for LegoSetListFragment.java

---
+++ app/src/main/java/albright/csit/legosetstracker/LegoSetListFragment.java
@@ -0,0 +1,15 @@
+/*______________________________________________________________________________
+   Created By:  Kevin M. Albright
+Creation Date:  11.16.2015
+
+  Modified By:  Kevin M. Albright
+Last Modified:  11.16.2015
+
+   Assignment:  Lego Sets Tracker
+    File Name:  LegoSetListFragment
+      Purpose:  
+______________________________________________________________________________*/
+package albright.csit.legosetstracker;
+
+public class LegoSetListFragment {
+}
