849c338c1c097f11cd35b8bf340d6c8d62d63cdf
King-Nivek <King-Nivek@users.noreply.github.com>
2015-11-17 16:04:22 -0500

    Changed Tab format. From 2 spaces to 4 spaces.

--- app/src/main/java/albright/csit/legosetstracker/LegoSet.java
+++ app/src/main/java/albright/csit/legosetstracker/LegoSet.java
@@ -13,105 +13,105 @@ ______________________________________________________________________________*/
 package albright.csit.legosetstracker;
 
 public class LegoSet {
-  ////  Fields
-  /////////////////////////////////////
-  private long autoId;
-  private String id;
-  private String name;
-  private long themeId;
-  private String themeName;
-  private int pieces;
-  private String acquiredDate;
-  private int quantity;
+    ////  Fields
+    /////////////////////////////////////
+    private long autoId;
+    private String id;
+    private String name;
+    private long themeId;
+    private String themeName;
+    private int pieces;
+    private String acquiredDate;
+    private int quantity;
 
-  ////  Setters
-  /////////////////////////////////////
-  public void setAutoId(long autoId) {
-    this.autoId = autoId;
-  }
+    ////  Setters
+    /////////////////////////////////////
+    public void setAutoId(long autoId) {
+        this.autoId = autoId;
+    }
 
-  public void setId(String id) {
-    this.id = id;
-  }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setThemeId(long themeId) {
-    this.themeId = themeId;
-  }
+    public void setThemeId(long themeId) {
+        this.themeId = themeId;
+    }
 
-  public void setThemeName(String themeName) {
-    this.themeName = themeName;
-  }
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
 
-  public void setPieces(int pieces) {
-    this.pieces = pieces;
-  }
+    public void setPieces(int pieces) {
+        this.pieces = pieces;
+    }
 
-  public void setAcquiredDate(String acquiredDate) {
-    this.acquiredDate = acquiredDate;
-  }
+    public void setAcquiredDate(String acquiredDate) {
+        this.acquiredDate = acquiredDate;
+    }
 
-  public void setQuantity(int quantity) {
-    this.quantity = quantity;
-  }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-  ////  Getters
-  /////////////////////////////////////
-  public long getAutoId() {
-    return autoId;
-  }
+    ////  Getters
+    /////////////////////////////////////
+    public long getAutoId() {
+        return autoId;
+    }
 
-  public String getId() {
-    return id;
-  }
+    public String getId() {
+        return id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public long getThemeId() {
-    return themeId;
-  }
+    public long getThemeId() {
+        return themeId;
+    }
 
-  public String getThemeName() {
-    return themeName;
-  }
+    public String getThemeName() {
+        return themeName;
+    }
 
-  public int getPieces() {
-    return pieces;
-  }
+    public int getPieces() {
+        return pieces;
+    }
 
-  public String getAcquiredDate() {
-    return acquiredDate;
-  }
+    public String getAcquiredDate() {
+        return acquiredDate;
+    }
 
-  public int getQuantity() {
-    return quantity;
-  }
+    public int getQuantity() {
+        return quantity;
+    }
 
-  ////  Constructors
-  /////////////////////////////////////
-  public LegoSet() {
-  }
+    ////  Constructors
+    /////////////////////////////////////
+    public LegoSet() {
+    }
 
-  public LegoSet(int autoId) {
-    this.autoId = autoId;
-  }
+    public LegoSet(int autoId) {
+        this.autoId = autoId;
+    }
 
-  public LegoSet(int autoId, String legoSetId, String legoSetName,
-                 long legoThemeId, String legoThemeName, int legoSetPieces,
-                 String legoSetAcquiredDate, int legoSetQuantity) {
+    public LegoSet(int autoId, String legoSetId, String legoSetName,
+                   long legoThemeId, String legoThemeName, int legoSetPieces,
+                   String legoSetAcquiredDate, int legoSetQuantity) {
 
-    this.autoId = autoId;
-    this.id = legoSetId;
-    this.name = legoSetName;
-    this.themeId = legoThemeId;
-    this.themeName = legoThemeName;
-    this.pieces = legoSetPieces;
-    this.acquiredDate = legoSetAcquiredDate;
-    this.quantity = legoSetQuantity;
-  }
+        this.autoId = autoId;
+        this.id = legoSetId;
+        this.name = legoSetName;
+        this.themeId = legoThemeId;
+        this.themeName = legoThemeName;
+        this.pieces = legoSetPieces;
+        this.acquiredDate = legoSetAcquiredDate;
+        this.quantity = legoSetQuantity;
+    }
 }
